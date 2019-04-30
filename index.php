bdbf01bd78ae04a354de3a64193884edd8e3bbf3
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-04-30 20:28:32 +0300

    rand() funkcija

--- index.php
+++ index.php
@@ -8,6 +8,14 @@
 <h1>Vardas - PHP su manim buvo ir <?php echo date("H", strtotime('-1 hours')) . " valanda!"; ?></h1>
 <p><?php echo "Sekantys metai " . date('Y', strtotime('+1 years')) .  " uz kalnu!"; ?></p>
 
+<p>Random funkcija</p>
+<p>PHP lydes ir <?php print date("Y-m-d", strtotime("+" . rand(0,10) . "years")) . "<br>";?></p>
+<p>Vardas - Galbut turesiu <?php print rand(1, 5) . " vaiku"  . "<br>";?></p>
+<p>D. Trump'as nebebus prezidentu <?php print date("Y-m-d", strtotime('+' . rand(2,12) . "years")) . "<br>" ;?></p>
+<p>antras budas</p>
+<p>D. Trump'as nebebus prezidentu <?php print rand(2021,2031) . "-" . date("m-d") . "<br>" ;?></p>
+
+
 
 
 </body>
