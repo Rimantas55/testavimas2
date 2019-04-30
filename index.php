8b87014a067aa5598a84f217691fe9ff0dccd405
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-04-30 19:06:05 +0300

    bandomo rasyti html  koda kartu su php

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
+<html>
 <head>
+    <meta charset="utf-8">
+    <title>As valdau HTML <?php echo "ir PHP"; ?></title>
 
 </head>
 <body>
-<h1>Vardas- HTML</h1>
-<p>Moku kurti statini vaizda</p>
+<h1>Vardas - HTML <?php echo "ir PHP assas/e!"; ?></h1>
+<p>Kuriu dinamini turini. Atidaryta <?php echo date('Y-m-d H:i:s'); ?></p>
+
 </body>
+
 </html>
+
+
+
+<?php
+//
+//print "Tekstas padarytas su php";
+//
+//print "<br>";
+//
+//print date('Y-m-d H:i:s');
+//
+//print "<br>";
+//
+//?>
+
+
+
