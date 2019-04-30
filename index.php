5cac097d6ab76a338e243f9ef911534e2be9c223
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-04-30 19:47:25 +0300

    strtotime() mokomes funcija

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>As, <?php echo "Antradienis ir PHP"; ?> </title>
+    <title>PHP lydes ir <?php echo "ryt " . date("Y-m-d", strtotime('+1 days')) ; ?> </title>
 </head>
 <body>
-<h1>Vardas - HTML <?php echo "ir PHP"; ?> asas/e jau nuo <?php echo date('Y'); ?></h1>
-<p>Viskas prasidejo <?php echo date('m') .  " menesio, " . date('d'). " diena!"; ?></p>
+<h1>Vardas - PHP su manim buvo ir <?php echo date("H", strtotime('-1 hours')) . " valanda!"; ?></h1>
+<p><?php echo "Sekantys metai " . date('Y', strtotime('+1 years')) .  " uz kalnu!"; ?></p>
+
+
 
 </body>
 
