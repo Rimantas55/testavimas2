ed1d90183713f9bfab56fd2a0b42ec15aff8eecb
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-04-30 20:44:19 +0300

    keiciam su rand() funkcija css style

--- index.php
+++ index.php
@@ -2,26 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>PHP lydes ir <?php echo "ryt " . date("Y-m-d", strtotime('+1 days')) ; ?> </title>
+
+    <style>
+        body {
+            background-color: rgb(<?php print rand(0,255) . "," . rand(0,255) . "," . rand(0,255); ?>);
+        }
+        h1 {
+            font-size: <?php print rand(1,30) . "pt";?>;
+        }
+    </style>
+    <title>PHP lydes ir <?php echo "ryt " . date("Y-m-d", strtotime('+1 days')); ?> </title>
 </head>
 <body>
 <h1>Vardas - PHP su manim buvo ir <?php echo date("H", strtotime('-1 hours')) . " valanda!"; ?></h1>
-<p><?php echo "Sekantys metai " . date('Y', strtotime('+1 years')) .  " uz kalnu!"; ?></p>
+<p><?php echo "Sekantys metai " . date('Y', strtotime('+1 years')) . " uz kalnu!"; ?></p>
 
 <p>Random funkcija</p>
-<p>PHP lydes ir <?php print date("Y-m-d", strtotime("+" . rand(0,10) . "years")) . "<br>";?></p>
-<p>Vardas - Galbut turesiu <?php print rand(1, 5) . " vaiku"  . "<br>";?></p>
-<p>D. Trump'as nebebus prezidentu <?php print date("Y-m-d", strtotime('+' . rand(2,12) . "years")) . "<br>" ;?></p>
+<p>PHP lydes ir <?php print date("Y-m-d", strtotime("+" . rand(0, 10) . "years")) . "<br>"; ?></p>
+<p>Vardas - Galbut turesiu <?php print rand(1, 5) . " vaiku" . "<br>"; ?></p>
+<p>D. Trump'as nebebus prezidentu <?php print date("Y-m-d", strtotime('+' . rand(2, 12) . "years")) . "<br>"; ?></p>
 <p>antras budas</p>
-<p>D. Trump'as nebebus prezidentu <?php print rand(2021,2031) . "-" . date("m-d") . "<br>" ;?></p>
+<p>D. Trump'as nebebus prezidentu <?php print rand(2021, 2031) . "-" . date("m-d") . "<br>"; ?></p>
 
-
+<p>keiciam spalvas su php</p>
 
 
 </body>
 
 </html>
-
 
 
 <?php
